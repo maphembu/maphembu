c9d957a03aafe3da17111d513312359b4d4b9e6c
maphembu <infomaphembu@gmail.com>
2025-03-14 20:00:26 +0200

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,51 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // AOS ANIMATIONS
+    AOS.init();
+
+    // NAVBAR
+    $('.navbar-nav .nav-link').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+    // NEWS IMAGE RESIZE
+    function NewsImageResize(){
+      $(".navbar").scrollspy({ offset: -76 });
+      
+      var LargeImage = $('.large-news-image').height();
+
+      var MinusHeight = LargeImage - 6;
+
+      $('.news-two-column').css({'height' : (MinusHeight - LargeImage / 2) + 'px'});
+    }
+
+    $(window).on("resize", NewsImageResize);
+    $(document).on("ready", NewsImageResize);
+
+    $('a[href*="#"]').click(function (event) {
+      if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: target.offset().top - 66
+          }, 1000);
+        }
+      }
+    });
+    
+  })(window.jQuery);
+
+  const today = new Date();
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    const formattedDate = today.toLocaleDateString('en-US', options);
+    document.getElementById('current-date').textContent = formattedDate;
+
+    
+
+  
